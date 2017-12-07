560c8135c4dd96806ab30331de77e960c7fc4dc9
Kiko <144036@via.dk>
2017-12-07 12:10:55 +0100

    updated isRange() method and its Junit test

--- SEP4 Project Artlinko/src/services/DataValidator.java
+++ SEP4 Project Artlinko/src/services/DataValidator.java
@@ -1,32 +1,43 @@
 package services;
 
+import java.util.ArrayList;
+
 import model.ResponseQA;
 
 public class DataValidator
 {
 
-   public boolean isDouble(ResponseQA response){
-      
-      String str = response.getAnswers().get(0);
-      
-        try  
-        {  
-          double d = Double.parseDouble(str);  
-        }  
-        catch(NumberFormatException nfe)  
-        {  
-          return false;  
-        }  
-        return true;  
-      
-   }
-   
- /*  public static void main(String[] args)
+   public boolean isDouble(ResponseQA response)
    {
-      
-      
-   //   ResponseQA res = new ResponseQA(answers, question); 
+
+      String str = response.getAnswers().get(0);
+
+      try
+      {
+         double d = Double.parseDouble(str);
+      }
+      catch (NumberFormatException nfe)
+      {
+         return false;
+      }
+      return true;
+
    }
-   */
-   
+
+   public boolean isRange(ResponseQA response)
+   {
+
+      String str = response.getAnswers().get(0);
+      String[] arr = str.split("-");
+      int length1 = arr[0].length();
+      int length2 = arr[1].length();
+
+      if ((arr.length != 2) || (length1 == 0) || (length2 == 0))
+         return false;
+      else if ((length1 == length2) || (length1 + 1 == length2))
+         return true;
+      else
+         return true;
+   }
+
 }
--- SEP4 Project Artlinko/src/tests/DataValidationTest.java
+++ SEP4 Project Artlinko/src/tests/DataValidationTest.java
@@ -13,17 +13,28 @@ import services.DataValidator;
 public class DataValidationTest
 {
    ArrayList<String> answers = new ArrayList<>();
-   boolean validated = false;
+   boolean validated1 = true;
+   boolean validated2 = true; 
    DataValidator dv = new DataValidator();
 
    @Test
    public void doubleValidation()
    {
-      answers.add("-12.5.12");
+      answers.add("-12.5");
       answers.add("-12");
       ResponseQA res = new ResponseQA(answers, "What's the temperature?"); 
       boolean result = dv.isDouble(res);
-      assertEquals(validated, result);
+      assertEquals(validated1, result);
+   }
+   
+   @Test
+   public void rangeValidation()
+   {
+      answers.add("11.2-12.5");
+      answers.add("-12");
+      ResponseQA res = new ResponseQA(answers, "What's the temperature?"); 
+      boolean result = dv.isRange(res);
+      assertEquals(validated2, result);
    }
 
 }
