bdd61e2d7f44e0a8fac1570bff95aefbe0fe6c97
alexsimaschevici <32359205+alexsimaschevici@users.noreply.github.com>
2017-12-11 09:47:36 +0100

    news

--- SEP4 Project Artlinko/src/model/SurveyResponsesCollection.java
+++ SEP4 Project Artlinko/src/model/SurveyResponsesCollection.java
@@ -7,9 +7,9 @@ import java.util.List;
 /**
  * Holds all the questions and responses of a survey from a current responder
  * 
- * @author Alexandru
+ * @author Alexandru, Cristian
  */
-<<<<<<< HEAD
+
 public class SurveyResponsesCollection {
 
 	List<ResponseQA> surveyResp;
@@ -41,89 +41,36 @@ public class SurveyResponsesCollection {
 		return surveyResp.size();
 	}
 	
-	//TO BE IMPLEMENTED
-	public List<ResponseQA> getColumn(String questionID){
-		return null;
-	}
+	  // TO BE IMPLEMENTED
+	   public List<ResponseQA> getColumn(String questionID)
+	   {
+	      ArrayList<ResponseQA> list = new ArrayList<ResponseQA>();
+	      for (ResponseQA elem : surveyResp)
+	         if (elem.getQuestionID().equals(questionID))
+	            list.add(elem);
+	      return list;
+	   }
+
+	   // TO BE IMPLEMENTED
+	   public List<ResponseQA> getRow(String surveyInstanceID)
+	   {
+	      ArrayList<ResponseQA> list = new ArrayList<ResponseQA>();
+	      for (ResponseQA elem : surveyResp)
+	         if (elem.getSurveyInstanceID().equals(surveyInstanceID))
+	            list.add(elem);
+	      return list;
+	   }
+
+	   // TO BE IMPLEMENTED
+	   public List<ResponseQA> getByProperty(HashMap<String, Boolean> properties)
+	   {
+	      ArrayList<ResponseQA> list = new ArrayList<ResponseQA>();
+	      for (ResponseQA elem : surveyResp)
+	         if (elem.property == properties)
+	            list.add(elem);
+	      return list;
+
+	   }
 	
-	
-	//TO BE IMPLEMENTED
-	public List<ResponseQA> getRow(String surveyInstanceID){
-		return null;
-	}
-	
-	//TO BE IMPLEMENTED
-	public List<ResponseQA> getByProperty(HashMap<String, Boolean> properties){
-		return null;
-	}
-	
-=======
-public class SurveyResponsesCollection
-{
 
-   List<ResponseQA> surveyResp;
-
-   public SurveyResponsesCollection()
-   {
-      super();
-      this.surveyResp = new ArrayList<ResponseQA>();
-   }
-
-   public List<ResponseQA> getSurveys()
-   {
-      return surveyResp;
-   }
-
-   public void setSurveys(List<ResponseQA> surveys)
-   {
-      this.surveyResp = surveys;
-   }
-
-   public void addSurvey(ResponseQA oneResp, int row)
-   {
-      surveyResp.add(oneResp);
-   }
-
-   public ResponseQA getSurvey(int pos)
-   {
-      return surveyResp.get(pos);
-   }
-
-   public int size()
-   {
-      return surveyResp.size();
-   }
-
-   // TO BE IMPLEMENTED
-   public List<ResponseQA> getColumn(String questionID)
-   {
-      ArrayList<ResponseQA> list = new ArrayList<ResponseQA>();
-      for (ResponseQA elem : surveyResp)
-         if (elem.getQuestionID().equals(questionID))
-            list.add(elem);
-      return list;
-   }
-
-   // TO BE IMPLEMENTED
-   public List<ResponseQA> getRow(String surveyInstanceID)
-   {
-      ArrayList<ResponseQA> list = new ArrayList<ResponseQA>();
-      for (ResponseQA elem : surveyResp)
-         if (elem.getSurveyInstanceID().equals(surveyInstanceID))
-            list.add(elem);
-      return list;
-   }
-
-   // TO BE IMPLEMENTED
-   public List<ResponseQA> getByProperty(HashMap<String, Boolean> properties)
-   {
-      ArrayList<ResponseQA> list = new ArrayList<ResponseQA>();
-      for (ResponseQA elem : surveyResp)
-         if (elem.property == properties)
-            list.add(elem);
-      return list;
-
-   }
-
->>>>>>> origin/master
 }
