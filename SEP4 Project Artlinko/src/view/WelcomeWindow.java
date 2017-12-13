1b60f14ac7422248b01beb8ec37e0a41c7399b01
alexsimaschevici <32359205+alexsimaschevici@users.noreply.github.com>
2017-12-13 13:57:42 +0100

    News

--- SEP4 Project Artlinko/src/view/WelcomeWindow.java
+++ SEP4 Project Artlinko/src/view/WelcomeWindow.java
@@ -32,7 +32,7 @@ import javax.swing.JProgressBar;
 
 import controller.SystemController;
 
-public class WelcomeWindow extends JFrame
+public class WelcomeWindow
 {
 
    private JPanel contentPane;
@@ -41,6 +41,7 @@ public class WelcomeWindow extends JFrame
    JLabel lblProgress = new JLabel("Progress:");
    JLabel progressStatusLabel = new JLabel("");
    SystemController contr;
+   JFrame frame;
 
 //   /**
 //    * Launch the application.
@@ -71,110 +72,119 @@ public class WelcomeWindow extends JFrame
    {
 	   
 	  this.contr= controller; 
-
-      setTitle("Welcome");
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      setBounds(100, 100, 576, 354);
-      contentPane = new JPanel();
-      contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-      setContentPane(contentPane);
-      contentPane.setLayout(null);
-
-      Label selectFilelbl = new Label("Select csv file:");
-      selectFilelbl.setFont(new Font("Dialog", Font.BOLD, 13));
-      selectFilelbl.setBounds(27, 28, 105, 29);
-      contentPane.add(selectFilelbl);
-
-      textField = new JTextField();
-      textField.setBounds(28, 61, 396, 29);
-      contentPane.add(textField);
-      textField.setColumns(10);
-
-      JButton browseBtn = new JButton("Browse...");
-      browseBtn.addActionListener(new ActionListener()
-      {
-         public void actionPerformed(ActionEvent arg0)
-         {
-            JFileChooser fc = new JFileChooser();
-            fc.setCurrentDirectory(new java.io.File("C:\\SCHOOL\\SEP4\\Resources\\Surveys\\Original_data.csv"));
-            fc.setDialogTitle("File Browser.");
-            fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-            if (fc.showOpenDialog(browseBtn) == JFileChooser.APPROVE_OPTION)
-            {
-               textField.setText(fc.getSelectedFile().getAbsolutePath());
-            }
-         }
-      });
-      browseBtn.setFont(new Font("Tahoma", Font.PLAIN, 12));
-      browseBtn.setBounds(455, 61, 94, 29);
-      contentPane.add(browseBtn);
-
-      JButton fetchBtn = new JButton("Fetch file");
-      ActionListener fetchFile = new ActionListener()
-      {
-         public void actionPerformed(ActionEvent e)
-         {
-            if (!textField.getText().isEmpty())
-            {
-               String path = textField.getText();
-               path = path.replace("\\", "/");
-               System.out.println(path);
-               contr.readSurveys(path);
-               
-               ListWindow listWin = new ListWindow();
-               listWin.setVisible(true);
-            }
-         }
-      };
-      
-      ActionListener nextFrame = new ActionListener()
-      {
-         public void actionPerformed(ActionEvent e)
-         {
-           
-         }
-      };
-
-      fetchBtn.addActionListener(fetchFile);
-      fetchBtn.setFont(new Font("Tahoma", Font.PLAIN, 12));
-      fetchBtn.setBounds(223, 110, 110, 29);
-      contentPane.add(fetchBtn);
-
-      lblProgress.setFont(new Font("Tahoma", Font.PLAIN, 13));
-      lblProgress.setBounds(142, 153, 60, 29);
-      contentPane.add(lblProgress);
-
-      progressStatusLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
-      progressStatusLabel.setBounds(200, 153, 192, 29);
-      contentPane.add(progressStatusLabel);
-
-      JButton restartBtn = new JButton("Restart");
-      restartBtn.addActionListener(fetchFile);
-      restartBtn.setFont(new Font("Tahoma", Font.PLAIN, 12));
-      restartBtn.setBounds(356, 197, 94, 29);
-      restartBtn.setEnabled(false); 
-      contentPane.add(restartBtn);
-      
-      JButton nextBtn = new JButton("Next");
-      nextBtn.addActionListener(nextFrame);
-      nextBtn.setFont(new Font("Tahoma", Font.PLAIN, 12));
-      nextBtn.setBounds(456, 197, 94, 29);
-      contentPane.add(nextBtn);
-      
-      
-      textArea.setForeground(new Color(255, 0, 0));
-      textArea.setBackground(UIManager.getColor("CheckBox.background"));
-      textArea.setLineWrap(true);
-
-      textArea.setBounds(27, 200, 396, 92);
-      contentPane.add(textArea);
-
-      JProgressBar progressBar = new JProgressBar();
-      progressBar.setStringPainted(true);
-      progressBar.setBounds(138, 28, 285, 29);
-      contentPane.add(progressBar);
-      progressStatusLabel.setText("0%");
-      this.setVisible(true);
+	  this.frame= new JFrame("");
+      drawFirstPage();
    }
+   
+   public void drawFirstPage(){
+	   	  frame.setTitle("Welcome");
+		  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		  frame.setBounds(100, 100, 576, 354);
+	      contentPane = new JPanel();
+	      contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+	      frame.setContentPane(contentPane);
+	      contentPane.setLayout(null);
+
+	      Label selectFilelbl = new Label("Select csv file:");
+	      selectFilelbl.setFont(new Font("Dialog", Font.BOLD, 13));
+	      selectFilelbl.setBounds(27, 28, 105, 29);
+	      contentPane.add(selectFilelbl);
+
+	      textField = new JTextField();
+	      textField.setBounds(28, 61, 396, 29);
+	      contentPane.add(textField);
+	      textField.setColumns(10);
+
+	      JButton browseBtn = new JButton("Browse...");
+	      browseBtn.addActionListener(new ActionListener()
+	      {
+	         public void actionPerformed(ActionEvent arg0)
+	         {
+	            JFileChooser fc = new JFileChooser();
+	            fc.setCurrentDirectory(new java.io.File("C:\\SCHOOL\\SEP4\\Resources\\Surveys\\Original_data.csv"));
+	            fc.setDialogTitle("File Browser.");
+	            fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+	            if (fc.showOpenDialog(browseBtn) == JFileChooser.APPROVE_OPTION)
+	            {
+	               textField.setText(fc.getSelectedFile().getAbsolutePath());
+	            }
+	         }
+	      });
+	      browseBtn.setFont(new Font("Tahoma", Font.PLAIN, 12));
+	      browseBtn.setBounds(455, 61, 94, 29);
+	      contentPane.add(browseBtn);
+
+	      JButton fetchBtn = new JButton("Fetch file");
+	      ActionListener fetchFile = new ActionListener()
+	      {
+	         public void actionPerformed(ActionEvent e)
+	         {
+	            if (!textField.getText().isEmpty())
+	            {
+	               String path = textField.getText();
+	               path = path.replace("\\", "/");
+	               System.out.println(path);
+	               contr.readSurveys(path);
+	               
+	               ListWindow listWin = new ListWindow();
+	               listWin.setVisible(true);
+	            }
+	         }
+	      };
+	      
+	      ActionListener nextFrame = new ActionListener()
+	      {
+	         public void actionPerformed(ActionEvent e)
+	         {
+	         
+	          frame.getContentPane().removeAll();
+	          frame.revalidate();
+	          frame.repaint();
+	         }
+	      };
+
+	      fetchBtn.addActionListener(fetchFile);
+	      fetchBtn.setFont(new Font("Tahoma", Font.PLAIN, 12));
+	      fetchBtn.setBounds(223, 110, 110, 29);
+	      contentPane.add(fetchBtn);
+
+	      lblProgress.setFont(new Font("Tahoma", Font.PLAIN, 13));
+	      lblProgress.setBounds(142, 153, 60, 29);
+	      contentPane.add(lblProgress);
+
+	      progressStatusLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
+	      progressStatusLabel.setBounds(200, 153, 192, 29);
+	      contentPane.add(progressStatusLabel);
+
+	      JButton restartBtn = new JButton("Restart");
+	      restartBtn.addActionListener(fetchFile);
+	      restartBtn.setFont(new Font("Tahoma", Font.PLAIN, 12));
+	      restartBtn.setBounds(356, 197, 94, 29);
+	      restartBtn.setEnabled(false); 
+	      contentPane.add(restartBtn);
+	      
+	      JButton nextBtn = new JButton("Next");
+	      nextBtn.addActionListener(nextFrame);
+	      nextBtn.setFont(new Font("Tahoma", Font.PLAIN, 12));
+	      nextBtn.setBounds(456, 197, 94, 29);
+	      contentPane.add(nextBtn);
+	      
+	      
+	      textArea.setForeground(new Color(255, 0, 0));
+	      textArea.setBackground(UIManager.getColor("CheckBox.background"));
+	      textArea.setLineWrap(true);
+
+	      textArea.setBounds(27, 200, 396, 92);
+	      contentPane.add(textArea);
+
+	      JProgressBar progressBar = new JProgressBar();
+	      progressBar.setStringPainted(true);
+	      progressBar.setBounds(138, 28, 285, 29);
+	      contentPane.add(progressBar);
+	      progressStatusLabel.setText("0%");
+	      frame.setVisible(true);
+   }
+   
+   
    
 }
